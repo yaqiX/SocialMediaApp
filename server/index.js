2427cc3907f99bc5c4dfaac0742b3703bb913aaa
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-01 18:11:13 -0700

    connect mongoDB

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
-app.morgan("*")
+app.use(morgan("common"))
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
-app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
 /* FILE STORAGE */
@@ -40,7 +40,23 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
+/* AUTH */
+app.post("/auth/register", upload.single("picture"), register);
 
 
+/* MONGOOSE */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
 
