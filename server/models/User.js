2a3279fef2f6a59c6110ab23dbb89eaff79a057a
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-01 18:25:04 -0700

    add UserSchema

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50
+        },
+        lastName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50
+        },
+        email: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 50
+        },
+        password: {
+            type: String,
+            require: true,
+            min: 6,
+        },
+        picturePath: {
+            type: String,
+            default: ""
+        },
+        friends: {
+            type: Array,
+            default: []
+        },
+        location: String,
+        occupation: String,
+        views: Number,
+        impressions: Number
+    },
+    { 
+        timestamps: true
+    }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
