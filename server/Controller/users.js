44c8c09b4f8f9e3efda59b9e5332a61644cfeecd
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-03 22:14:38 -0700

    add read and update for user

--- server/Controller/users.js
+++ server/Controller/users.js
@@ -1,16 +1,5 @@
 import User from "../models/User";
 
-
-const friend = {
-    firstName,
-    lastName,
-    email,
-    password,
-    picturePath,
-    friends,
-    location,
-    occupation
-}
 /* READ */
 export const getUser = async (req, res) =>{
     try {
@@ -40,4 +29,38 @@ export const getUserFriends = async (req, res) => {
         } catch (error) {
         res.status(404).json({ message: error.message })
     }
+};
+
+
+/* UPDATE */
+export default addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filer((idOfFriends) => idOfFriends !== friendId);
+            friend.friends = friend.friends.filer((idOfUser) => idOfUser !== id)
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        )
+
+        const friendsList = friends.map(({_id, firstname, lastname, occupation, location, picturePath}) => {
+            return {_id, firstname, lastname, occupation, location, picturePath}})
+            
+        res.status(200).json(friendsList);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
+
