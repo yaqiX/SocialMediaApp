852f7e57ed0b92b516d915095b73eecaada0bcb5
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-05 12:24:37 -0800

    set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
+import LoginPage from "scenes/loginPage";
 
-import './App.css';
 
 const App = () => {
+
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}/>
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/profile/:userId" element={<ProfilePage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
