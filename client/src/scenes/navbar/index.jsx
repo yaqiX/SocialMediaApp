cdcf8ef3b0382bfaef137b1e8d5dbcc16d59b2d0
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-05 20:25:35 -0800

    update mui Box, add navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
+
+    const [isMobileMenuToggled, setisMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch(); 
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user)
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px");
+
+    const theme = useTheme();
+    const neutralLightTheme = theme.palette.neutral.light;
+    const darkTheme = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const lightTheme = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
     return(
-        <div>
-            Navbar
-        </div>
+        <FlexBetween padding="1.5rem 6%" backgroundColor={alt}>
+            {/* can pass because it's Box */}
+            
+        </FlexBetween>
     )
 };
 
