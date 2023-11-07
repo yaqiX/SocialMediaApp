809f532c0797860d3452813357034ef3e1236df8
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-06 21:49:30 -0800

    add submit and switch login function

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -175,8 +175,56 @@ const Form = () => {
                                             }
                                         </Dropzone>
                                     </Box>
+                                    <TextField
+                                        label="Email"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.email}
+                                        name="email"
+                                        error={Boolean(touched.email) && Boolean(errors.email)}
+                                        helperText={touched.email && errors.email}
+                                        sx={{gridColumn:"span 1"}}
+                                    />
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.password}
+                                        name="password"
+                                        error={Boolean(touched.password) && Boolean(errors.password)}
+                                        helperText={touched.password && errors.password}
+                                        sx={{gridColumn:"span 1"}}
+                                    />
                                 </>
                             )}
+                        </Box>
+                        <Box>
+                            <Button
+                                type="submit"
+                                sx={{
+                                    m:"2rem",
+                                    backgroundColor: palette.primary.main,
+                                    color:palette.background.alt,
+                                    "&:hover":{color:palette.primary.main, cursor:"pointer"}
+                                }}
+                            >
+                                {isLogin ? "LOGIN" : "REGISTER"}
+                            </Button>
+                            <Typography
+                                onClick={()=>{
+                                    setPageType(isLogin ? "register" : "login")
+                                    resetForm();
+                                }}
+                                sx={{
+                                    textDecoration:"underline",
+                                    color:palette.primary.main,
+                                    "&:hover":{color:palette.primary.light, cursor:"pointer"}
+                                }}
+                            >
+                                {isLogin ? "Don't have an account? Sign Up here." : 
+                                            "Already have an account? Login here."}
+                            </Typography>
                         </Box>
                     </form>
                 )
