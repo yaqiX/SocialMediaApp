d86d320719f5f7fec3190cde99855b71d2785b3e
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-11-06 21:13:05 -0800

    add register schema

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -10,5 +10,27 @@ import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import * as yup from "yup"
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+import { paste } from "@testing-library/user-event/dist/paste";
+
+const registerSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string("Invalid Email").required("required"),
+    password: yup.string().required("required"),
+    location: yup.string().required("required"),
+    occupation: yup.string().required("required"),
+    picture: yup.string().required("required"),
+})
+
+
+
+
+
+
+
 
 
